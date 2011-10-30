575589c9ab3d21a20c5f0d67d3ec6161f0a31e5b
Gosuke Miyashita <gosukenator@gmail.com>
2011-10-30 20:18:42 +0900

    handle appropriately forbidden resources

--- app/controllers/photo_controller.rb
+++ app/controllers/photo_controller.rb
@@ -3,5 +3,11 @@ class PhotoController < ApplicationController
     @media    = Insta.media_item(session[:access_token], params[:id])
     @likers   = Insta.media_likes(session[:access_token], params[:id])
     @comments = Insta.media_comments(session[:access_token], params[:id])
+
+    if not @media
+      @message = "You cannot view this resource."
+      render template: 'error/error', status: 400
+      return
+    end
   end
 end
