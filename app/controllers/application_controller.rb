d66e8c924fbd066afd1e130b876337380c72afdf
Gosuke Miyashita <gosukenator@gmail.com>
2012-03-23 22:37:49 +0900

    Ensure access_token is mine certainly

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,18 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  before_filter :check_token
+
+  protected
+
+  def check_token
+    return unless session[:username]
+    client = Instagram.client(:access_token => session[:access_token])
+    if session[:username] != client.user["username"]
+      session[:username]        = nil
+      session[:user_id]         = nil
+      session[:profile_picture] = nil
+      session[:access_token]    = nil
+    end
+  end
+
 end
